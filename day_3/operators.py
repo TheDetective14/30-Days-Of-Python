efdcba0185c80ce1706e1441aea42152d2dd5bf2
Dan Gochuico <79511016+TheDetective14@users.noreply.github.com>
2024-10-01 16:22:41 +0800

    Create operators.py

---
+++ day_3/operators.py
@@ -0,0 +1,30 @@
+# 1-3
+
+age = int("18")
+my_height = float("57.5")
+complex = 5j
+
+# 4
+
+base = int(input("Enter base: "))
+height = int(input("Enter height: "))
+area = 0.5*base*height
+print("The area of the triangle is", area)
+
+# 5
+
+side_a = int(input("Enter side a: "))
+side_b = int(input("Enter side b: "))
+side_c = int(input("Enter side c: "))
+perimiter = side_a + side_b + side_c
+print("The perimiter of the triangle is", perimiter)
+
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+rect_area = length*width
+rect_perimiter = 2*(length*width)
+print("The area of the triangle is", area)
+
+radius = float(input("Radius: "))
+circle_area = 3.14*radius**2
+circumference = 2*3.14*radius
